04ce66b3b8a3ed40e6126e8122cc190830d193fc
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-06-28 17:14:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(say)
   if say == "I LOVE YOU GRANDMA!"
-    "I LOVE YOU TOO PUMPKING!"
+    "I LOVE YOU TOO PUMPKIN!"
     elsif say == say.upcase
     "NO, NOT SINCE 1938!"
   else
