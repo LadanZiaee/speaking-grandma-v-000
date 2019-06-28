32e32444b4ae06a9d3d4066527521fb7b320c6a6
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-06-28 16:57:37 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,9 +2,9 @@
 def speak_to_grandma(say)
   if say.upcase
     "NO, NOT SINCE 1938!"
-    elsif say == "I LOVE YOU GRANDMA!"
+   elsif say == "I LOVE YOU GRANDMA!"
       "ILOVE YOU TOO PUMPKIN!"
-    else
+  else
     "HUH?! SPEAK UP, SONNY!"
   end
 end
